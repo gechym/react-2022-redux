69e17edf4b77eba60ef10bb5be06f6931c776214
ndbao <nguyenducbao166@gmail.com>
2022-05-27 15:44:53 +0700

    Add fake database

--- src/App.js
+++ src/App.js
@@ -5,29 +5,12 @@ import store from '~/Redux/store';
 function App() {
     const [state, setState] = useState(store.getState());
 
-    const handleUpdateStore = () => {
-        const action = { type: 'updateName', name: 'gechym😂' };
-        store.dispatch(action);
-    };
-
-    const handleUpdateStoreAge = () => {
-        const action = { type: 'updateAge', age: '10🙌' };
-        store.dispatch(action);
-    };
-
     useEffect(() => {
         store.subscribe(() => {
             setState(store.getState());
         });
     });
 
-    return (
-        <div className="App">
-            <button onClick={handleUpdateStore}>Click me 😊</button>
-            <button onClick={handleUpdateStoreAge}>Click me😘</button>
-            <h1>Hello {state.nameState.name}</h1>
-            <h1>Age {state.ageState.age}</h1>
-        </div>
-    );
+    return <div className="App"></div>;
 }
 export default App;
