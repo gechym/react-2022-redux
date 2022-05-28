2bf976d29d1590731f28fe75f62d72eda4e2e380
ndbao <nguyenducbao166@gmail.com>
2022-05-29 00:07:30 +0700

    cách 3

--- src/Redux/toolkit/useSlice.js
+++ src/Redux/toolkit/useSlice.js
@@ -10,143 +10,81 @@ const initialState = {
 const useSlice = createSlice({
     name: 'users',
     initialState,
-    reducers: {
-        fetch_request: (state, action) => {
-            return {
-                ...state,
-                loading: true,
-            };
-        },
-        fetch_success: (state, action) => {
-            return {
-                ...state,
-                loading: false,
-                error: undefined,
-                data: action.payload,
-            };
-        },
-        fetch_error: (state, action) => {
-            return {
-                ...state,
-                loading: false,
-                error: action.payload,
-            };
-        },
-        create_request: (state, action) => {
-            return {
-                ...state,
-                loading: true,
-            };
-        },
-        create_success: (state, action) => {
-            return {
-                ...state,
-                loading: false,
-                error: undefined,
-                data: [action.payload, ...state.data],
-            };
-        },
-        create_error: (state, action) => {
-            return {
-                ...state,
-                loading: false,
-                error: action.payload,
-            };
-        },
-        update_request: (state, action) => {
-            return {
-                ...state,
-                loading: true,
-            };
-        },
-        update_success: (state, action) => {
-            return {
-                ...state,
-                loading: false,
-                data: state.data?.map((item) =>
-                    item.id === action.payload.id ? action.payload : item,
-                ),
-            };
-        },
-        update_error: (state, action) => {
-            return {
-                ...state,
-                loading: false,
-                error: action.payload,
-            };
-        },
-        delete_request: (state, action) => {
-            return {
-                ...state,
-                loading: true,
-            };
-        },
-        delete_success: (state, action) => {
-            return {
-                ...state,
-                loading: false,
-                data: state.data?.filter((item) => item.id !== action.payload),
-            };
-        },
-        delete_error: (state, action) => {
-            return {
-                ...state,
-                loading: false,
-                error: action.payload,
-            };
-        },
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addMatcher(
+                (action) => {
+                    return (
+                        action.type.startsWith('users/') &&
+                        action.type.endsWith('/pending')
+                    );
+                },
+                (state, action) => {
+                    state.loading = true;
+                },
+            )
+            .addMatcher(
+                (action) => {
+                    return (
+                        action.type.startsWith('users/') &&
+                        action.type.endsWith('/fulfilled')
+                    );
+                },
+                (state, action) => {
+                    state.loading = false;
+                    state.data = action.payload;
+                },
+            )
+            .addMatcher(
+                (action) => {
+                    return (
+                        action.type.startsWith('users/') &&
+                        action.type.endsWith('/rejected')
+                    );
+                },
+                (state, action) => {
+                    state.loading = false;
+                    state.error = action.error;
+                },
+            );
     },
 });
 
 export const getUser = createAsyncThunk(
     'users/getUsers',
     async (payload, thunkAPi) => {
-        thunkAPi.dispatch(userActions.fetch_request());
-        try {
-            const res = await useApi.getUsers();
-            thunkAPi.dispatch(userActions.fetch_success(res));
-        } catch (error) {
-            thunkAPi.dispatch(userActions.fetch_error(error.message));
-        }
+        const res = await useApi.getUsers();
+        return res;
     },
 );
 
 export const createUser = createAsyncThunk(
     'users/createUser',
     async (payload, thunkAPi) => {
-        thunkAPi.dispatch(userActions.create_request());
-        try {
-            const res = await useApi.createUser(payload);
-            thunkAPi.dispatch(userActions.create_success(res));
-        } catch (error) {
-            thunkAPi.dispatch(userActions.create_error(error.message));
-        }
+        const res = await useApi.createUser(payload);
+        const state = thunkAPi.getState().userState;
+        return [res, ...state.data];
     },
 );
 
 export const deleteUser = createAsyncThunk(
     'users/deleteUser',
     async (payload, thunkAPi) => {
-        thunkAPi.dispatch(userActions.delete_request());
-        try {
-            await useApi.deleteUser(payload);
-            thunkAPi.dispatch(userActions.delete_success(payload));
-        } catch (error) {
-            thunkAPi.dispatch(userActions.delete_error(error.message));
-        }
+        await useApi.deleteUser(payload);
+        const state = thunkAPi.getState().userState;
+        return state.data.filter((user) => user.id !== payload);
     },
 );
 
 export const updateUser = createAsyncThunk(
     'users/updateUser',
     async (payload, thunkAPi) => {
-        thunkAPi.dispatch(userActions.update_request());
-        try {
-            await useApi.updateUser(payload);
-            thunkAPi.dispatch(userActions.update_success(payload));
-        } catch (error) {
-            thunkAPi.dispatch(userActions.update_error(error.message));
-        }
+        await useApi.updateUser(payload);
+        const state = thunkAPi.getState().userState;
+        return state.data.map((user) =>
+            user.id === payload.id ? payload : user,
+        );
     },
 );
 
